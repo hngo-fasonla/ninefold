fd387c5015305fe537216987452afd3bcdd044a4
hngo <hngo.opensource@gmail.com>
2014-10-29 17:25:09 +0700

    Init
    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ninefold::Application.config.secret_key_base = '671c2da977ef44997ee9b7f16f8b009fed141169a6cd0f3a8fdbd0559a2bb88b9fcb9a56d4635e772773a986f13fe06450ae8e24ba8c059427a1356e1c6ea94e'
